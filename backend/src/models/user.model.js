f0c2c78c0f82fe70eaa8ec099450a1a1d70d222c
anshumaanbaaghi <ganumumbai067@gmail.com>
2024-02-11 14:37:39 +0530

    Define MongoDB User Schema structure

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (email) {
+          return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+        },
+        message: "Invalid Email Format",
+      },
+    },
+    avatar: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    passwordUpdatedAt: {
+      type: Date,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpires: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = new mongoose.model("User", userSchema);
+
+module.exports = { User };
