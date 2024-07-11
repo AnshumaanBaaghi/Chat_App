bc56481badd60e0ca1892001f6c47c6acc1f06bd
anshumaanbaaghi <ganumumbai067@gmail.com>
2024-07-11 12:25:20 +0530

    feat: Created video call component to display users' video

--- frontend/src/components/videocall/OneOnOneVc.jsx
+++ frontend/src/components/videocall/OneOnOneVc.jsx
@@ -1,17 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { ReactIcon } from "./ReactIcon";
+import React, { useCallback, useEffect, useState } from "react";
+import { ReactIcon } from "../ReactIcon";
 import { IoVideocam } from "react-icons/io5";
 import { IoMdMic } from "react-icons/io";
-import { Button } from "./ui/button";
+import { Button } from "../ui/button";
 import ReactPlayer from "react-player";
+import { useSelector } from "react-redux";
+import { initialiseVc_api } from "@/api";
 
-export const OneOnOneVc = ({ setIsOnCall }) => {
+export const OneOnOneVc = ({ setIsOnCall, selectedChat }) => {
+  const socket = useSelector((state) => state.socket.socket);
+  const loggedinUser = useSelector((state) => state.user.userDetail);
+
   const [myStream, setMyStream] = useState(null);
 
   const handleEndCall = () => {
     myStream.getTracks().forEach((track) => track.stop());
+    setMyStream(null);
     setIsOnCall(false);
   };
+
   const initialiseStream = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({
@@ -23,23 +30,25 @@ export const OneOnOneVc = ({ setIsOnCall }) => {
       console.log("error:", error);
     }
   };
+
   useEffect(() => {
     initialiseStream();
   }, []);
 
   return (
     <div className="fixed w-full fullHeight bg-red-300">
-      <div className="h-[90%] bg-blue-500 flex gap-5 p-5">
-        <div className="w-full h-full border border-red-500 rounded-3xl overflow-hidden">
+      <div className="h-[90%] bg-blue-500 flex gap-5 p-5 items-center">
+        <div className="w-full h-full relative rounded-3xl overflow-hidden">
           <ReactPlayer
             playing
             muted
             width="100%"
             height="100%"
             url={myStream}
+            className="outline-none"
           />
+          <span className="absolute bottom-3 left-3 text-white ">You</span>
         </div>
-
         {/* <ReactPlayer playing muted width="100%" height="100%" url={myStream} /> */}
       </div>
       <div className="h-[10%] bg-green-500 flex justify-center items-center gap-2">
